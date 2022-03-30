12b1365a2144190650190ed95a2b0e1a8f5e3d4c
Welliton Luiz Becker <chaank18@gmail.com>
2022-03-30 19:14:12 -0300

    Alterado

---
+++ Confin/Controle/Conexao.cs
@@ -0,0 +1,36 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confin.Controle {
+    public class Conexao {
+
+        public static NpgsqlConnection GetConexao() {
+            NpgsqlConnection conexao = null;
+
+            try {
+                conexao = new NpgsqlConnection("Server=localhost;Port=5432;User Id=postgres;Password=postgres; Database=financeiro;");
+                conexao.Open();
+            } catch(Exception e) {
+                Console.WriteLine("Erro ao concectar o banco:" + e.Message);
+            }
+
+            return conexao;
+        }
+
+        public static void SetFechaConexao(NpgsqlConnection conexao) {
+            if(conexao != null) {
+                try {
+                    conexao.Close();
+                } catch(Exception e) {
+                    Console.WriteLine("Erro ao fechar conexão banco:" + e.Message);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Confin/frmPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using Confin.Controle;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Confin {
+    public partial class frmPrincipal : Form {
+
+        NpgsqlConnection conexao = null;
+
+        public frmPrincipal() {
+            conexao = Conexao.GetConexao();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void frmPrincipal_FormClosed(object sender, FormClosedEventArgs e) {
+            Conexao.SetFechaConexao(conexao);
+        }
+    }
+}
